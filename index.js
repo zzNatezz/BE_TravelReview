f57406ac692b11e240e08e9b3d3b5345560f3757
zzD3chzz <thcdzch@gmail.com>
2024-05-22 00:05:14 +0700

    ss

--- index.js
+++ index.js
@@ -14,9 +14,19 @@ const sv = express();
 
 sv.use(express.json());
 const corsOptions = {
-  origin: "https://travel-review-web.vercel.app",
+  origin: "*",
   credentials: true,
 };
+
+sv.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 sv.use(cors(corsOptions));
 
