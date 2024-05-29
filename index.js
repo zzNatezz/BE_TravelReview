813fa12204af2b2f20b3fe3b7baf38f1f6d86e78
zzD3chzz <thcdzch@gmail.com>
2024-05-29 23:21:59 +0700

    S

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 };
 
 sv.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
